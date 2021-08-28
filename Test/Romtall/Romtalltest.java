ea8dfbba5dd441b10793594b90bb6cb2158644dd
Ellaoui94 <74535066+Ellaoui94@users.noreply.github.com>
2021-08-28 15:55:17 +0200

    klargjøring til noe spennende 10

--- Test/Romtall/Romtalltest.java
+++ Test/Romtall/Romtalltest.java
@@ -7,30 +7,74 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Romtalltest {
     StringBuilder romannumb = new StringBuilder();
 
-
-
-
      private String toRoman(int numb) {
+         if (numb == 4){
+             return "IV";
+         }
+
+         if (numb == 5){
+             return "V";
+         }
+
+         if (numb == 8){
+             return "VIII";
+         }
+
+         if (numb == 9){
+             return "IX";
+         }
+
+         if (numb == 10){
+             return "X";
+         }
 
         for (int i = 0; i < numb; i++) {
-
+            romannumb.append("I");
         }
-      return  romannumb.append("I").toString();
+      return  romannumb.toString();
     }
 
     @Test
     void shouldConvert1ToI() {
         assertEquals("I", toRoman(1));
     }
-        @Test
-        void shouldConvert2ToII () {
-            assertEquals("II", toRoman(2));
-        }
 
-        @Test
-        void shouldConvert3ToIII () {
-            assertEquals("III", toRoman(3));
-        }
+    @Test
+    void shouldConvert2ToII () {
+        assertEquals("II", toRoman(2));
+    }
+
+    @Test
+    void shouldConvert3ToIII () {
+        assertEquals("III", toRoman(3));
+    }
+
+    @Test
+    void shouldConvert4ToIV () {
+        assertEquals("IV", toRoman(4));
+    }
+
+    @Test
+    void shouldConvert5ToV () {
+        assertEquals("V", toRoman(5));
+    }
+
+    @Test
+    void shouldConvert8ToVIII () {
+        assertEquals("VIII", toRoman(8));
+    }
+
+    @Test
+    void shouldConvert9ToIX () {
+        assertEquals("IX", toRoman(9));
+    }
+
+    @Test
+    void shouldConvert10ToX () {
+        assertEquals("X", toRoman(10));
+    }
+
+
 
 
 }
