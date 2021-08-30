93862639c4d969e50afbce9e5a981ebcfdecd703
Ellaoui94 <74535066+Ellaoui94@users.noreply.github.com>
2021-08-30 14:13:06 +0200

    alle oppgavene er ferdige, eksempel tatt fra google

--- Test/Romtall/Romtalltest.java
+++ Test/Romtall/Romtalltest.java
@@ -5,77 +5,55 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class Romtalltest {
-    StringBuilder romannumb = new StringBuilder();
-
-     private String toRoman(int numb) {
-         if (numb == 4){
-             return "IV";
-         }
-
-         if (numb == 5){
-             return "V";
-         }
-
-         if (numb == 8){
-             return "VIII";
-         }
-
-         if (numb == 9){
-             return "IX";
-         }
-
-         if (numb == 10){
-             return "X";
-         }
-
-        for (int i = 0; i < numb; i++) {
-            romannumb.append("I");
-        }
-      return  romannumb.toString();
-    }
 
     @Test
     void shouldConvert1ToI() {
-        assertEquals("I", toRoman(1));
+        assertEquals("I", romTallConvert.toRoman(1));
     }
 
     @Test
     void shouldConvert2ToII () {
-        assertEquals("II", toRoman(2));
+        assertEquals("II", romTallConvert.toRoman(2));
     }
 
     @Test
     void shouldConvert3ToIII () {
-        assertEquals("III", toRoman(3));
+        assertEquals("III", romTallConvert.toRoman(3));
     }
 
     @Test
     void shouldConvert4ToIV () {
-        assertEquals("IV", toRoman(4));
+        assertEquals("IV", romTallConvert.toRoman(4));
     }
 
     @Test
     void shouldConvert5ToV () {
-        assertEquals("V", toRoman(5));
+        assertEquals("V", romTallConvert.toRoman(5));
     }
 
     @Test
     void shouldConvert8ToVIII () {
-        assertEquals("VIII", toRoman(8));
+        assertEquals("VIII", romTallConvert.toRoman(8));
     }
 
     @Test
     void shouldConvert9ToIX () {
-        assertEquals("IX", toRoman(9));
+        assertEquals("IX", romTallConvert.toRoman(9));
     }
 
     @Test
     void shouldConvert10ToX () {
-        assertEquals("X", toRoman(10));
+        assertEquals("X", romTallConvert.toRoman(10));
     }
 
+    @Test
+    void shouldConvert32ToXII () {
+        assertEquals("XXXII", romTallConvert.toRoman(32));
+    }
 
-
-
+    @Test
+    void shouldConvert37ToXXXVII () {
+        assertEquals("XXXVII", romTallConvert.toRoman(37));
+    }
 }
 
--- src/Romtall/romTallConvert.java
+++ src/Romtall/romTallConvert.java
@@ -1,6 +1,46 @@
 package Romtall;
 
 public class romTallConvert {
-    private void toRoman(int i) {
+
+    public static String toRoman(int numb) {
+        StringBuilder romannumb = new StringBuilder();
+        /* if (numb == 4){
+             return "IV";
+         }
+
+         if (numb == 5){
+             return "V";
+         }
+
+         if (numb == 8){
+             return "VIII";
+         }
+
+         if (numb == 9){
+             return "IX";
+         }
+
+         if (numb == 10){
+             return "X";
+         }
+
+        for (int i = 0; i < numb; i++) {
+            romannumb.append("I");
+        }
+      return  romannumb.toString();*/
+
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romanLiterals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        for (int i = 0; i < values.length; i++) {
+            while (numb >= values[i]) {
+
+                numb -= values[i];
+
+                romannumb.append(romanLiterals[i]);
+            }
+
+        }
+        return romannumb.toString();
     }
 }
